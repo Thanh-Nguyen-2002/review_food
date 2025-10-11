a5db04d6a90b1debdc6cda01634c909135519a8e
Thanh-Nguyen-2002 <nxthanh.nb2002@gmail.com>
2025-10-11 20:16:24 +0700

    update SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,25 @@ import { ToastContainer } from "react-toastify";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: "Review Food",
-    description: "Trang đánh giá món ăn",
+    title: "ReviewFood | Khám Phá Ẩm Thực Việt",
+    description:
+            "ReviewFood mang đến những câu chuyện, review và hành trình khám phá ẩm thực Việt Nam.",
+    openGraph: {
+        title: "ReviewFood | Hành trình ẩm thực Việt",
+        description:
+            "Cùng ReviewFood thưởng thức hương vị ba miền Bắc – Trung – Nam.",
+        url: "https://review-food-pe9tmbl99-nguyenthanhs-projects-9bd44689.vercel.app/",
+        images: [
+            {
+                url: "/about_us.jpg",
+                width: 1200,
+                height: 630,
+                alt: "ReviewFood Vietnam",
+            },
+        ],
+    },
 };
+
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
